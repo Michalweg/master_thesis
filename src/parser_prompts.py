829ab572ec3e7ef50b7917d6f29c4bbc863fae34
Michalweg <michalwegrzynowski33@gmail.com>
2025-06-29 21:41:16 +0200

    feature: removing prompt for triplet extraction to the other location

--- src/parser_prompts.py
+++ src/parser_prompts.py
@@ -256,27 +256,6 @@ Part of a research paper:
 Assistant:
 """
 
-
-triplets_extraction_prompt_gpt_4 = """
-Human: Act as data analyst and extract all the tuples including the name of the task addressed in the paper, utilized datasets and evaluation metrics from the uploaded document in the JSON format. 
-Please use json format for each different tuple. Example format: [{{"Task": "Task name", "Dataset": "Dataset name", "Metric": "Metric name"}}]. 
-Your answer will immediately start with the json object satisfying the given template and contain nothing else.
-
-Follow below instructions: 
-<instructions>
-1. If you cannot find any of such tuples, return empty string without any explanation.
-2. If you cannot find all of required fields (task, dataset, metric) please do not output it into the response. You must
-provide only tuples with all required fields!
-3. Your answer will immediately start with the json object satisfying the given template and contain nothing else.
-4. In terms of dataset, please specify only the main name of dataset, do not provide additional notes such as "CoNLL 2003 shared task", but rather "CoNLL 2003". 
-Also provide specific dataset name avoid extraction things like "validation dataset". 
-5. In terms of metric, do not output things like "Sate of the Art", provide specific metric related to given task an dataset.
-6. In metric section, please provide just one metric for each tuple. If you see multiple metrics, please create tuple for each of them. 
-</instructions>
-
-Assistant:
-"""
-
 system_prompt_for_triplet_extraction_gpt4o = \
 """
 You are a helpful assistant. 
